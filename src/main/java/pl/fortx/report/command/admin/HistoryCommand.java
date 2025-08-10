e683f20c4bc611f36ee729435fcd5d71761b6988
BTZ-CODE <balthasarpascal@gmail.com>
2025-08-10 16:45:14 +0200

    Added HistoryCommand.class

---
+++ src/main/java/pl/fortx/report/command/admin/HistoryCommand.java
@@ -0,0 +1,24 @@
+package pl.fortx.report.command.admin;
+
+import lombok.RequiredArgsConstructor;
+import org.bukkit.entity.Player;
+import org.incendo.cloud.annotations.Argument;
+import org.incendo.cloud.annotations.Command;
+import org.incendo.cloud.annotations.Permission;
+import org.jetbrains.annotations.NotNull;
+import pl.fortx.report.config.MessagesConfig;
+import pl.fortx.report.config.PluginConfig;
+
+
+@RequiredArgsConstructor
+public class HistoryCommand {
+    private final @NotNull PluginConfig pluginConfig;
+    private final @NotNull MessagesConfig messages;
+
+    @Permission("report.history")
+    @Command("history|h|reports <player>")
+    public void run(@NotNull final Player player, @Argument(value = "player", description = "The player whose report history you want to view") final @NotNull Player target) {
+        // Will add the implementation later
+    }
+
+}
